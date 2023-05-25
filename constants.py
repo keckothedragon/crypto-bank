4d5b8a02883ab24ae6aaad621e527a830877590b
keckothedragon <flappystar05@gmail.com>
2023-05-24 21:20:04 -0400

    Quick formatting fix

--- constants.py
+++ constants.py
@@ -11,7 +11,7 @@ pickleFilePath = "./confirmation.pickle"
 helpMsg = ["Commands:", # to make easier to read
 f"{prefix}showCrypto [name]: Shows standings for a specific crypto.",
 f"{prefix}addCrypto [target] [amount]: Adds amount of your own crypto to a user (do not ping them in the message).",
-f"{prefix}createCrypto [name]: Creates a crypto with the specified name. If you already own a crypto, you will not be able to make another."
+f"{prefix}createCrypto [name]: Creates a crypto with the specified name. If you already own a crypto, you will not be able to make another. Crypto names cannot contain spaces",
 f"{prefix}deleteCrypto: Deletes the users crypto, asking first for confirmation or cancellation.",
 f"{prefix}confirmDeletion: Confirms deletion after deleteCrypto was called.",
 f"{prefix}cancelDeletion: Cancels deletion after deleteCrypto was called.",
