d4311561bfe7aa7e7311a793918629eeae8b71e9
keckothedragon <flappystar05@gmail.com>
2023-05-25 10:24:56 -0400

    Added listCrypto command and changed dialogues to be more helpful

--- crypto.py
+++ crypto.py
@@ -36,7 +36,7 @@ async def showCrypto(ctx, crypto=""):
         return
     coins = json_read(constants.jsonFilePath)
     if crypto not in coins:
-        await ctx.send("Invalid crypto name.")
+        await ctx.send(f"Invalid crypto name. To see a list of all crypto, use {constants.prefix}listCrypto.")
         return
     bank = coins[crypto]["Bank"]
     bank = [[value, key] for key, value in bank.items()]
@@ -61,7 +61,7 @@ async def addCrypto(ctx, target="", val=0):
         if ctx.author.id == value["Owner"]:
             userCoin = key
     if userCoin == "":
-        await ctx.send("You do not own a crypto.")
+        await ctx.send(f"You do not own a crypto. You can create one with {constants.prefix}createCrypto.")
         return
     if target in coins[userCoin]["Bank"]:
         coins[userCoin]["Bank"][target] += val
@@ -91,6 +91,15 @@ async def createCrypto(ctx, cryptoName=""):
 
 @commands.Command
 async def deleteCrypto(ctx):
+    coins = json_read(constants.jsonFilePath)
+    userOwn = False
+    for coin in coins:
+        if coins[coin]["Owner"] == ctx.author.id:
+            userOwn = True
+    if not userOwn:
+        await ctx.send(f"You do not own a crypto. You can create one with {constants.prefix}createCrypto.")
+        return
+
     confirmation: set = pickle_read(constants.pickleFilePath)
     if ctx.author.id in confirmation:
         await ctx.send(f"You still have an unconfirmed command. Please run {constants.prefix}confirmDeletion to confirm or {constants.prefix}cancelDeletion to cancel.")
@@ -101,7 +110,6 @@ async def deleteCrypto(ctx):
 
 @commands.Command
 async def confirmDeletion(ctx):
-    """a"""
     confirmation: set = pickle_read(constants.pickleFilePath)
     if ctx.author.id not in confirmation:
         await ctx.send(f"There is nothing awaiting confirmation.")
@@ -128,7 +136,14 @@ async def cancelDeletion(ctx):
     confirmation.remove(ctx.author.id)
     pickle_write(constants.pickleFilePath, confirmation)
     await ctx.send(f"Cancelled deletion.")
-    
+
+@commands.Command
+async def listCrypto(ctx):
+    coins = json_read(constants.jsonFilePath)
+    msg = "\n".join([str(coin) for coin in coins])
+    if not msg:
+        msg = "No crypto to list."
+    await ctx.send(msg)
 
 @commands.Command
 async def cryptoHelp(ctx):
@@ -140,7 +155,7 @@ client.add_command(createCrypto)
 client.add_command(deleteCrypto)
 client.add_command(confirmDeletion)
 client.add_command(cancelDeletion)
-# client.help_command.no_category = ""
 client.add_command(cryptoHelp)
+client.add_command(listCrypto)
 
 client.run(os.getenv("TOKEN"))
